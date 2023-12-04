747f25df949f3febbdf5c186eda6f09d80d2b2f2
mohashalaby <mohamedshalaby2@aol.com>
2023-12-04 23:38:26 +0200

    34456

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,9 +3,8 @@ def add (a , b):
     a = 1
     b = 2
     return a + b
-    print("{} + {} = {} ".formmat(a, b, return)
-            import add_0
-            form add_0 import add 
-            add(1,2)
+import add_0
+add_0.add(1,2)
+print("{} + {} = {} ".format(a, b, return) 
 
 

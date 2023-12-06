3d41e6f71117cd3ece00b9c3bb4e94a3d5020d27
mohashalaby <mohamedshalaby2@aol.com>
2023-12-07 00:16:44 +0200

    345678

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+f __name__ == "__main__":
+import sys
+
+total = 0
+for i in range(len(sys.argv) - 1):
+total += int(sys.argv[i + 1]
+        print("{}".format(total))
+                                    i

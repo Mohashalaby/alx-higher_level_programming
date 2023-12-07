4b32fd6abd8bb9bed2872acee51cee6934a12098
mohashalaby <mohamedshalaby2@aol.com>
2023-12-08 00:06:12 +0200

    ewewewe

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < o and idx > len(my_list):
+    if idx < o :
         return None
+    elif idx >= len(my_list):
+        return None
+    return my_list[idx]
